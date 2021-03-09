13ccbe3592059a074f05a21725b479dc7a892a51
Thiago Henrique <thiagohenrique776@gmail.com>
2021-03-09 19:08:43 -0300

    CPF Validation

--- src/index.js
+++ src/index.js
@@ -9,13 +9,20 @@ const customers = [];
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists) {
+        return response.status(400).json({error: "Customer already exists!"});
+    }
     
-    const id = uuidv4();
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
